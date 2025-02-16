a2555cb40d47ca10d28b879ba994cdc749e52550
geongeongeon <leegeonhee0204@gmail.com>
2025-02-16 21:46:00 +0900

    lecture02 - 코틀린에서 null을 다루는 방법

---
+++ src/lecture02.kt
@@ -0,0 +1,35 @@
+import sub.Person
+
+fun main() {
+    // null을 허용한 변수에서 null 확인을 안하고 값을 사용할 경우
+    val str1: String? = "ABC"
+    println(str1?.length)
+
+    // null을 허용한 변수에서 null 확인을 안하고 사용할 때 결과가 null이 나왔을 경우에 특정 값으로 반환
+    val str2: String? = "ABC"
+    println(str2?.length ?: 0)
+
+    val person = Person("공부하는 개발자")
+    println(person.name)
+}
+
+fun startsWithA1(str: String?): Boolean {
+    return str?.startsWith("A") ?: throw IllegalArgumentException("null 값이 들어왔습니다.")
+}
+
+fun startsWithA2(str: String?): Boolean? {
+    return str?.startsWith("A")
+}
+
+fun startsWithA3(str: String?): Boolean {
+    return str?.startsWith("A") ?: false
+}
+
+// 해당 값이 절대 null이 될 수 없을 경우
+fun startsWithA4(str: String?): Boolean {
+    return str!!.startsWith("A")
+}
+
+fun startsWithA5(str: String): Boolean {
+    return str.startsWith("A")
+}
---
+++ src/sub/Person.kt
@@ -0,0 +1,6 @@
+package sub
+
+// var 변수 : 자동으로 getter, setter 생성
+// val 변수 : 자동으로 getter 생성
+class Person(val name: String?) {
+}
