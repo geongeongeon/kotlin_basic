b5ab0da2b1927c0dd1603d54a0339c41699bdb8b
geongeongeon <leegeonhee0204@gmail.com>
2025-02-17 18:41:35 +0900

    lecture07 - 코틀린에서 예외를 다루는 방법

---
+++ src/lec07/lecture07.kt
@@ -0,0 +1,22 @@
+package lec07
+
+fun main() {
+    val printer1 = FilePrinter()
+    printer1.readFileV1()
+}
+
+fun parseIntOrThrowV1(str: String): Int {
+    try {
+        return str.toInt()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("주어진 ${str}은 숫자가 아닙니다.")
+    }
+}
+
+fun parseIntOrThrowV2(str: String): Int? {
+    return try {
+        str.toInt()
+    } catch (e: NumberFormatException) {
+        null
+    }
+}
